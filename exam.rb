da047ba99b6f463131a0718c58c353110fb123e4
箱崎章朗 <hakozakifumiaki@hakozakiakiraakiranoMacBook-Air.local>
2022-11-28 16:27:55 +0900

    .ab

---
+++ exam.rb
@@ -0,0 +1,18 @@
+require 'webrick'  #インストールされたwebrickを呼び出し
+
+#webrickのインスタンスを生成、serverという名前のローカル変数に入れる
+server = WEBrick::HTTPServer.new({
+    :DocumentRoot => '.',
+    :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+    :Port => '3000',
+})
+
+['INT','TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+
+
+server.mount('/exam',WEBrick::HTTPServlet::ERBHandler, 'exam.html.erb')
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+server.mount('/goya1.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya1.rb')
+server.start
